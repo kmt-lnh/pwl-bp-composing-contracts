dcfb2e328224b64e1a75941c1046d0213fdb50de
kmt-lnh <kmt-lnh@users.noreply.github.com>
2017-07-18 15:24:26 +0200

    implementing the Date related parts of the paper
    the add function has a weird type mismatch, will
    investigate later

---
+++ Date.hs
@@ -0,0 +1,36 @@
+import Data.Time
+import Data.Decimal
+
+type Date = UTCTime
+
+date :: String -> Date
+date = parseTimeOrError True defaultTimeLocale dateFormat
+  where dateFormat = "%Y-%m-%d-%H:%M-%Z"
+
+t1 = date "2010-01-01-15:30-GMT"
+t2 = date "2010-02-01-12:00-GMT"
+
+-- instead of floats, we should be using Decimals integer
+-- part means days decimal part means seconds
+-- type Days = Decimal
+
+type Days = Double
+
+-- mimicking the behaviour of double, but can be extended to
+-- the proper (day,second) handling as well (subtracting the
+-- floor of diff then add the remaining seconds...
+diff :: Date -> Date -> Days
+diff d1 d2 = realToFrac $ (diffUTCTime d1 d2 ) / secondsInDay
+  where secondsInDay = (24 * 60 * 60)
+
+secondsInDay = (24 * 60 * 60)
+
+-- strange: the where clause works in ghci line-by-line but not when loaded from file
+-- add :: Date -> Days -> Date
+-- add d1 days = addUTCTime seconds d1
+--   where
+--     secondsInDay = (24 * 60 * 60)
+--     daysInSeconds  = fromIntegral (floor days) * secondsInDay
+--     remainingSeconds = fromIntegral (truncate (fromIntegral secondsInDay * (days - fromIntegral (floor days))))
+--     seconds = daysInSeconds + remainingSeconds
+
